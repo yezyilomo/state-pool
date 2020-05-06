1f658b753430487890948d0f404f26bf12546a39
yezy <yezileliilomo@hotmail.com>
2020-05-07 00:37:01 +0300

    :white_check_mark:Update tests

--- test/useGlobalState.test.js
+++ test/useGlobalState.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
-import { useGlobalState, createGlobalstate } from '../src/';
+import { store, useGlobalState } from '../src/';
 
 
-const count = createGlobalstate(0);
+store.init({});
+store.setState("count", 0);
 
 test('should update count', () => {
-    const { result } = renderHook(() => useGlobalState(count))
+    const { result } = renderHook(() => useGlobalState("count"))
 
     act(() => {
-        result.current[1](1)
+        result.current[1](count => 1)
     })
 
     expect(result.current[0]).toStrictEqual(1)
 })
-
