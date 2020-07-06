db481fe0ddac2f36d7f5356e869f0ce167e1dbf5
yezy <yezileliilomo@hotmail.com>
2020-07-07 02:12:51 +0300

    ✅Add test for store.clear

---
+++ test/store.clear.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { store, useGlobalState } from '../src/';
+
+
+store.setState("count", 0);
+
+test('should clear the entire global state and initialize `count` with 5 and `age` with 18', () => {
+    const hook1 = renderHook(() => useGlobalState("count"))
+
+    act(() => {
+        hook1.result.current[1](count => 1)
+    })
+
+    const hook2 = renderHook(() => useGlobalState("age", {default: 18}));
+
+    act(() => {
+        hook2.result.current[1](age => age + 2)
+    })
+
+    act(() => {
+        store.clear(()=>{ store.setState("count", 5); })
+    })
+
+    expect(hook1.result.current[0]).toStrictEqual(5)
+
+    expect(hook2.result.current[0]).toStrictEqual(18)
+})
+
+
+
