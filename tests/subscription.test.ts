55ab5a574f58dedfbe58dd9db51f05d12e2fe964
yezyilomo <yezileliilomo@hotmail.com>
2022-11-27 01:21:52 +0200

    ✅Update tests

--- tests/subscription.test.ts
+++ tests/subscription.test.ts
@@ -1,27 +1,63 @@
 import React from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
-import { createStore } from '../src/';
+import { createGlobalstate, useGlobalState, createStore } from '../src/';
 
 
-const store = createStore();
-store.setState("count", 0);
+const count = createGlobalstate(0);
 
 let testVal1 = 0;
-let testVal2 = 0;
 
-test('should update testVal1 & testVal2 through subscribers', () => {
-    const { result } = renderHook(() => store.useState("count"))
+test('should update testVal1 through a subscriber', () => {
+    const { result } = renderHook(() => useGlobalState(count))
 
     act(() => {
-        store.subscribe((key, value) => {
-            testVal1 = 1
-        })
-        store.getState("count").subscribe((value) => {
-            testVal2 = 2
+        count.subscribe((value) => {
+            testVal1 = ++testVal1;
         })
         result.current[1](count => 1)
     })
 
     expect(testVal1).toStrictEqual(1);
+})
+
+
+const user = createGlobalstate({ name: "Yezy", weight: 65 });
+
+let testVal2 = 0;
+
+test('should increment testVal2 twice through subscribers', () => {
+    const { result } = renderHook(() => useGlobalState(user))
+
+    act(() => {
+        user.select(user => user.weight).subscribe((value) => {
+            testVal2 = ++testVal2;
+        })
+
+        user.select(user => user.name).subscribe((value) => {
+            testVal2 = ++testVal2;
+        })
+
+        result.current[2](user => { user.weight = 66; user.name = "Ilomo"; });
+    })
+
     expect(testVal2).toStrictEqual(2);
+})
+
+
+const store = createStore();
+store.setState("count", 0);
+
+let testVal3 = 0;
+
+test('should update testVal3 through subscribers', () => {
+    const { result } = renderHook(() => store.useState("count"))
+
+    act(() => {
+        store.subscribe((key, value) => {
+            testVal3 = ++testVal3;
+        })
+        result.current[1](count => 1)
+    })
+
+    expect(testVal3).toStrictEqual(1);
 })
